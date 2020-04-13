14ec34d317be41d1b48ef1933e0f9e2569b7f589
Manuel Occorso <10528607@polimi.it>
2020-04-13 19:17:13 +0200

    1) Folder Refactoring 2) Options Page (without options) 3) fixed some small bugs

---
+++ src/options/options.js
@@ -0,0 +1,11 @@
+
+console.log("NM.option : RUNNING");
+console.log(document);
+
+
+let page = document.getElementById('options_div');
+constructOptions(page);
+
+function constructOptions(page) {
+  page.innerHTML = " Building with script ";
+}
