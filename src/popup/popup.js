bf85063429b135aaf50480e514b9a082bbd30717
Manuel Occorso <10528607@polimi.it>
2020-04-14 00:37:24 +0200

    Added interface to store codes in the extension permanently. Real Implementation is missing.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,6 @@
 console.log("NM.popup : Running");
 
+
 //MESSAGES
   //Request the math codes from the content
   var NOTION_MATH_RETRIEVE_MATH_CODES = "find_maths";
@@ -9,21 +10,45 @@ console.log("NM.popup : Running");
   var NOTION_MATH_CODES_UPDATED = "codes_updated";
 
 
-// ANCHOR LINKS
-  var custom_codes_div_anchor = "#custom_codes_div_anchor";
-  var page_codes_div_anchor = "#page_codes_div_anchor";
-
 // --------------------- VARS ----------------------------------------
 var custom_codes = [];
 var page_codes = [];
+var saved_codes = [];
 
 // --------------------- UTILITIES -----------------------------
 
-//----- translate a code id used in the textarea
+//----- translate a block id used
 //                   to the one used for the preview
-function preview_id_from_code_id(id){
-  return "label_" + id.toString();
+function preview_id(id){
+  return "preview_" + id.toString();
 }
+
+//----- translate a block id used
+//                   to the one used for the preview
+function textarea_id(id){
+  return "textarea_" + id.toString();
+}
+
+function save_btn_id(id){
+    return "save_btn_" + id.toString();
+}
+
+function delete_btn_id(id){
+    return "delete_btn_" + id.toString();
+}
+
+function gttop_btn_id(id){
+    return "gttop_btn_" + id.toString();
+}
+
+
+function clean_id(custom_id){
+  return custom_id.replace("label_","").replace("textarea_","")
+                  .replace("save_btn_","").replace("delete_btn_","")
+                  .replace("gttop_btn_","");
+}
+
+
 
 //----- send a request for the codes in the content
 function send_request_for_codes(){
@@ -41,16 +66,15 @@ function send_request_for_codes(){
 function getPosition(element){
         var e = document.getElementById(element);
         var y = 0;
-
         do{
             y += e.offsetTop;
         }while(e = e.offsetParent);
-
         return y;
     }
 
+
 function add_custom_katex_code() {
-  let new_id = '_' + Math.random().toString(36).substr(2, 9);
+  let new_id = Math.random().toString(36).substr(2, 9);
   custom_codes.push(
         {
           id : new_id,
@@ -60,16 +84,29 @@ function add_custom_katex_code() {
     );
   refresh_codes_visualization();
 }
+function add_saved_katex_code(code) {
+  let new_id = Math.random().toString(36).substr(2, 9);
+  saved_codes.push(
+        {
+          id : new_id,
+          code : code
+
+        }
+    );
+  refresh_codes_visualization();
+}
 
 function sync_katex_code(id, code){
   let label_area = document.getElementById(
-                            preview_id_from_code_id(id)
+                            preview_id(id)
                             );
   katex.render(code,
               label_area,
               { throwOnError: false}
             );
 }
+
+//-------- LISTENERS
 
 function add_listeners_to_text_area(text_area){
   text_area.addEventListener(
@@ -99,6 +136,27 @@ function add_listeners_to_text_area(text_area){
   );
 }
 
+function add_listeners_to_delete_btn(btn){
+
+
+}
+
+function add_listeners_to_save_btn(btn){
+  // TODO: STORE IN CHROME A NEW BLOCK WITH THE SAME ID
+  btn.addEventListener(
+    'click',
+    function()  {
+      let id = clean_id(btn.id);
+      let code = document.getElementById(textarea_id(id)).value;
+      add_saved_katex_code(code);
+    }
+  );
+
+}
+
+function add_listeners_to_gttop_btn(btn){
+  window.scrollTo(0,0);
+}
 
 chrome.runtime.onMessage.addListener(popup_receiver);
 function popup_receiver(request, sender, sendResponse){
--- src/popup/view.js
+++ src/popup/view.js
@@ -1,8 +1,21 @@
 
+var page_codes_div_id = "page_codes_div_id";
+var custom_codes_div_id = "custom_codes_div_id";
+var saved_codes_div_id = "saved_codes_div_id";
+
+
+var page_block_type = "page_block_type";
+var custom_block_type = "custom_block_type";
+var saved_block_type = "saved_block_type";
+
+var custom_codes_div_anchor = "#custom_codes_div_anchor";
+var page_codes_div_anchor = "#page_codes_div_anchor";
+var saved_codes_div_anchor = "#page_codes_div_anchor";
+
 //----- Create a TextArea from the Katex_code
 function set_text_area_with_code(code, id){
   let text_area = document.createElement("TEXTAREA");
-  text_area.id = id;
+  text_area.id = textarea_id(id);
   text_area.value = code;
   add_listeners_to_text_area(text_area);
   return text_area;
@@ -10,7 +23,7 @@ function set_text_area_with_code(code, id){
 
 function create_preview_paragraph(code, id ){
   let label_area = document.createElement("p");
-  label_area.id = preview_id_from_code_id(id);
+  label_area.id = preview_id(id);
   label_area.className +=  "rendered-code";
   katex.render(code,
               label_area,
@@ -49,23 +62,64 @@ function append_big_divider(node){
   append_new_line(node);
 }
 
-function append_code_block(node, code, index, custom){
+function append_gttop_btn(node, id){
+  let gttop_btn = document.createElement("BUTTON");
+  gttop_btn.id = gttop_btn_id(id);
+  gttop_btn.innerHTML =  ' <span class="front"> <img class="small-icon" '+
+                              ' src="popup/icons/up-multi-size.ico" '+
+                              ' alt="Return To Top"> </span>';
+  add_listeners_to_gttop_btn(gttop_btn);
+  node.append(gttop_btn);
+}
+
+function append_save_block_btn(node, id){
+  let save_block_btn = document.createElement("BUTTON");
+  save_block_btn.id = save_btn_id(id);
+  save_block_btn.innerHTML =  ' <span class="front"> <img class="small-icon" '+
+                              ' src="popup/icons/save-multi-size.ico" '+
+                              ' alt="Save"> </span>';
+  add_listeners_to_save_btn(save_block_btn);
+  node.append(save_block_btn);
+}
+
+
+function append_delete_block_btn(node, id){
+  let delete_block_btn = document.createElement("BUTTON");
+  delete_block_btn.id = delete_btn_id(id);
+  delete_block_btn.innerHTML =  ' <span class="front"> <img class="small-icon" '+
+                              ' src="popup/icons/delete-multi-size.ico" '+
+                              ' alt="Delete"> </span>';
+  add_listeners_to_delete_btn(delete_block_btn);
+  node.append(delete_block_btn);
+}
+
+
+
+function append_code_block(node, code, id, type){
+  id = id + type;
   let li = document.createElement("LI");
   node.append(li);
     let spoiler_div = document.createElement("DETAILS");
     li.append(spoiler_div);
       let summary = document.createElement("SUMMARY");
       spoiler_div.append(summary);
-        let preview_paragraph = create_preview_paragraph(code, index);
+        let preview_paragraph = create_preview_paragraph(code, id);
         summary.append(preview_paragraph);
-      let text_area = set_text_area_with_code(autoformatting(code), index);
+      let text_area = set_text_area_with_code(autoformatting(code), id);
       spoiler_div.append(text_area);
+
+      append_gttop_btn(spoiler_div, id);
+      append_save_block_btn(spoiler_div, id);
+      if(type === custom_block_type || type === saved_block_type ){
+        append_delete_block_btn(spoiler_div, id);
+      }
+
 }
 
-function append_code_blocks(node, codes, starting_id){
+function append_code_blocks(node, codes, type){
   for(let index = 0; index < codes.length; index++){
     let code = codes[index];
-    append_code_block(node, code.code, code.id, false);
+    append_code_block(node, code.code, code.id, type);
   }
 }
 
@@ -74,15 +128,21 @@ function append_code_blocks(node, codes, starting_id){
 function refresh_codes_visualization(){
 
   //insert the codes from content
-  let page_codes_div = document.getElementById('page_codes');
+  let page_codes_div = document.getElementById(page_codes_div_id);
   page_codes_div.innerHTML = "";
   append_title(page_codes_div, "3", "Notion Page Katex Codes", page_codes_div_anchor);
-  append_code_blocks(page_codes_div,page_codes, 0);
+  append_code_blocks(page_codes_div,page_codes, page_block_type);
 
   //insert customly created codes
-  let custom_codes_div = document.getElementById('custom_codes');
+  let custom_codes_div = document.getElementById(custom_codes_div_id);
   custom_codes_div.innerHTML = "";
   append_title(custom_codes_div, "3", "Custom Katex Codes", custom_codes_div_anchor);
-  append_code_blocks(custom_codes_div, custom_codes, page_codes.length);
+  append_code_blocks(custom_codes_div, custom_codes, custom_block_type);
+
+  //insert customly created codes
+  let saved_codes_div = document.getElementById(saved_codes_div_id);
+  saved_codes_div.innerHTML = "";
+  append_title(saved_codes_div, "3", "Saved Katex Codes", saved_codes_div_anchor);
+  append_code_blocks(saved_codes_div, saved_codes, saved_block_type);
 
 }
